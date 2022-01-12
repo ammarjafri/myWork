14ab825fd870399070d5758c26896435d3d7a67e
ammarjafri <75732757+ammarjafri@users.noreply.github.com>
2022-01-12 22:49:24 +0500

    update

--- amm/scan.py
+++ amm/scan.py
@@ -1,4 +1,4 @@
-import cv2, datetime, time, gpiozero
+import cv2, datetime, time, gpiozero, pysftp, threading
 import numpy as np
 import utlis
 buzz = gpiozero.LED(17)
@@ -23,13 +23,28 @@ dims = {
     "1080":(1920,1080),
     }
 
-saveImage = home_path+"imgs/"
+save_path = home_path+"imgs/"
 cap = cv2.VideoCapture(1)
 widthImg,heightImg = dims["1600"] 
 cap.set(3,widthImg)
 cap.set(4,heightImg)
 i=0
 
+def upload_file(filename,file_to_send):
+    while True:
+        try:
+            FTP_HOST, FTP_USER, FTP_PASS = ["52.214.240.108","sftp","P)O(I*U&A!S@D#F$@123"] 
+            filename_where_send = '/motion_box/'+filename
+            with pysftp.Connection(host=FTP_HOST, username=FTP_USER, password=FTP_PASS) as sftp:
+                print("connected")
+                sftp.put(file_to_send,filename_where_send)
+            break
+        except:
+            pass
+
+def my_thread(tar,arg=[]):
+    threads = threading.Thread(target=tar,args=arg)
+    threads.start()
 
 while True:
     success, img = cap.read()
@@ -49,11 +64,13 @@ while True:
         if differ >= 2.2:
             tic=toc
             time_now = datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
-            save = hubid+'_'+time_now+'.jpeg'
+            filename = hubid+'_'+time_now+'.jpeg'
+            file_save = save_path+filename
             # print(save)
             i=0
             # cv2.imwrite(save,img)
             # buzz.on()
+            # my_thread(upload_file,[filename,file_save])
             # time.sleep(2)
             # buzz.off()
     # tic=toc
